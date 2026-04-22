638a9aee3995bd39161b44d2463fb646fe7c7925
muh-furqon <37769126+muh-furqon@users.noreply.github.com>
2026-04-22 10:33:10 +0700

    revision

--- arena.c
+++ arena.c
@@ -1,39 +1,39 @@
 #include "arena.h"
-#include <stdio.h>
+#include <stdio.h> 
 
-void arena_init(Arena *a, uint8_t *buffer, size_t capacity) {
-    a->buffer = buffer;
+void arena_init(Arena *a, uint8_t *buffer_memory, size_t capacity) {
+    a->buffer = buffer_memory;
     a->capacity = capacity;
-    a->curr_offset = 0; //Mulai dari 0 ya
+    a->curr_offset = 0; // Mulai dari titik nol
 }
 
 size_t arena_alloc(Arena *a, size_t size) {
-    // Validation: Apakah arena masih bisa menampung data baru?
+    // Validasi: Apakah sisa lapangan masih muat untuk data baru?
     if (a->curr_offset + size > a->capacity) {
-        fprintf(stderr, "ERROR: Arena Overflow!");
-        return(size_t)-1; // Return nilai maksimum sebagai penanda error
+        fprintf(stderr, "ERROR: Arena Out of Memory!\n");
+        return (size_t)-1; // Mengembalikan nilai maksimum sebagai penanda error
     }
 
-    //Simpan offset sekarang untuk dikembalikan ke memory request
+    // Simpan offset saat ini untuk dikembalikan kepada peminta memori
     size_t allocated_offset = a->curr_offset;
-
-    // Geser garis batas (bump) sejauh ukuran yang direquest
-    a->curr_offset += size;
+    
+    // Geser garis batas (bump) sejauh ukuran yang diminta
+    a->curr_offset += size; 
 
     return allocated_offset;
 }
 
 void *arena_get(Arena *a, size_t offset) {
-    // Validation keamanan tambahan
-    if (offset >= a->curr_offset) {
+    // Validasi keamanan tambahan
+    if (offset >= a->capacity) {
         return NULL;
     }
-
-    // Return pointer asli dengan menjumlahkan alamat awal dan offset
+    
+    // Mengembalikan pointer asli dengan menjumlahkan alamat awal dan offset
     return (void *)(a->buffer + offset);
 }
 
 void arena_reset(Arena *a) {
-    // Reset O(1): Reset offset ke 0
+    // Efek reset O(1): Cukup kembalikan offset ke 0.
     a->curr_offset = 0;
 }
